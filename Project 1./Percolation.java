e1bceefd28d7e56ceaec688d2068a6b732195c45
Mo-Salamah <53323195+Mo-Salamah@users.noreply.github.com>
2021-08-09 18:58:54 -0400

    Update Percolation.java

--- Project 1./Percolation.java
+++ Project 1./Percolation.java
@@ -1,9 +1,13 @@
-/* *****************************************************************************
- *  Name:              Alan Turing
- *  Coursera User ID:  123456
- *  Last modified:     1/1/2019
- **************************************************************************** */
-
+/**
+ * Percolation. Given a composite systems comprised of randomly distributed
+ * insulating and metallic materials: what fraction of the materials need to be
+ * metallic so that the composite system is an electrical conductor? Given a
+ * porous landscape with water on the surface (or oil below), under what
+ * conditions will the water be able to drain through to the bottom (or the oil
+ * to gush through to the surface)? Scientists have defined an abstract process
+ * known as percolation to model such situations.
+ *
+ */
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
